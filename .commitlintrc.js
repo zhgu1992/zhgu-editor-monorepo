367760dd0cbdf7088ec6fa9766ea60a249054e70
zhgu <821829052@qq.com>
2025-06-04 21:51:43 +0800

    feat: 新增commit规范

---
+++ .commitlintrc.js
@@ -0,0 +1,18 @@
+// .commitlintrc.js
+// https://www.npmjs.com/package/@commitlint/config-conventional
+// [
+//   'build',
+//   'chore',
+//   'ci',
+//   'docs',
+//   'feat',
+//   'fix',
+//   'perf',
+//   'refactor',
+//   'revert',
+//   'style',
+//   'test'
+// ];
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+};
